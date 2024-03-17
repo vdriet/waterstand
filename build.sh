ef1488cf3e07d44f9c1bc29e38bcd2ddd949aeaf
Peter <peter@kokosnoot>
2024-03-17 21:14:11 +0100

    Versie voor upload naar TestPyPI

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-pip install -r requirements.txt
+pip install -i https://test.pypi.org/simple/ -r requirements.txt
 pip install setuptools pip --upgrade
 pip list --outdated
 pylint *.py
